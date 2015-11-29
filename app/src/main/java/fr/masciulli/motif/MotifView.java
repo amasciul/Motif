ad22374c51bb8d8638a2263184e627d4a73ae174
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2015-11-29 22:32:03 +0100

    Draw triangles in 4 different colors

--- app/src/main/java/fr/masciulli/motif/MotifView.java
+++ app/src/main/java/fr/masciulli/motif/MotifView.java
@@ -2,7 +2,6 @@ package fr.masciulli.motif;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
@@ -16,8 +15,11 @@ import java.util.Random;
 public class MotifView extends View {
     private static final int BOX_SIZE_PX = 150;
 
-    private final Paint darkPaint = new Paint();
-    private final Paint lightPaint = new Paint();
+    private final Paint blackPaint = new Paint();
+    private final Paint paint1 = new Paint();
+    private final Paint paint2 = new Paint();
+    private final Paint paint3 = new Paint();
+    private final Paint paint4 = new Paint();
 
     List<List<Point>> points = new ArrayList<>();
 
@@ -37,8 +39,10 @@ public class MotifView extends View {
     }
 
     private void initPaints() {
-        lightPaint.setColor(getContext().getResources().getColor(R.color.purple));
-        darkPaint.setColor(getContext().getResources().getColor(R.color.purple_dark));
+        paint2.setColor(getContext().getResources().getColor(R.color.purple));
+        paint1.setColor(getContext().getResources().getColor(R.color.purple_dark));
+        paint3.setColor(getContext().getResources().getColor(R.color.purple_darker));
+        paint4.setColor(getContext().getResources().getColor(R.color.purple_darkest));
     }
 
 
@@ -74,53 +78,28 @@ public class MotifView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-//        drawPoints(canvas);
-//        drawLines(canvas);
-//        drawGrid(canvas);
         drawTriangles(canvas);
+//        drawPoints(canvas);
+//        drawGrid(canvas);
     }
 
     private void drawGrid(Canvas canvas) {
         int x = BOX_SIZE_PX;
         while (x < getWidth()) {
-            canvas.drawLine(x, 0, x, getHeight(), darkPaint);
+            canvas.drawLine(x, 0, x, getHeight(), blackPaint);
             x += BOX_SIZE_PX;
         }
 
         int y = BOX_SIZE_PX;
         while (y < getHeight()) {
-            canvas.drawLine(0, y, getWidth(), y, darkPaint);
+            canvas.drawLine(0, y, getWidth(), y, blackPaint);
             y += BOX_SIZE_PX;
-        }
-    }
-
-    private void drawLines(Canvas canvas) {
-        for (int i = 0; i < points.size(); i++) {
-            for (int j = 0; j < points.get(0).size(); j++) {
-                Point point = points.get(i).get(j);
-                if (i > 0) {
-                    Point left = points.get(i - 1).get(j);
-                    canvas.drawLine(left.x, left.y, point.x, point.y, darkPaint);
-                    if (j > 0) {
-                        Point leftTop = points.get(i - 1).get(j - 1);
-                        canvas.drawLine(leftTop.x, leftTop.y, point.x, point.y, darkPaint);
-                    }
-                    if (j < points.get(0).size() - 1) {
-                        Point leftBottom = points.get(i - 1).get(j + 1);
-                        canvas.drawLine(leftBottom.x, leftBottom.y, point.x, point.y, darkPaint);
-                    }
-                }
-                if (j < points.get(0).size() - 1) {
-                    Point bottom = points.get(i).get(j + 1);
-                    canvas.drawLine(bottom.x, bottom.y, point.x, point.y, darkPaint);
-                }
-            }
         }
     }
 
     private void drawTriangles(Canvas canvas) {
         for (int i = 0; i < points.size(); i++) {
-            for (int j = 0; j < points.get(0).size(); j++) {
+            for (int j = i % 2; j < points.get(0).size(); j += 2) {
                 Point point = points.get(i).get(j);
                 if (i > 0 && j > 0) {
                     Point left = points.get(i - 1).get(j);
@@ -131,18 +110,40 @@ public class MotifView extends View {
                     path.lineTo(left.x, left.y);
                     path.lineTo(top.x, top.y);
                     path.close();
-                    canvas.drawPath(path, darkPaint);
+                    canvas.drawPath(path, paint1);
                 }
                 if (i > 0 && j < points.get(0).size() - 1) {
                     Point left = points.get(i - 1).get(j);
-                    Point bottomLeft = points.get(i - 1).get(j + 1);
+                    Point bottom = points.get(i).get(j + 1);
                     Path path = new Path();
                     path.setFillType(Path.FillType.EVEN_ODD);
                     path.moveTo(point.x, point.y);
                     path.lineTo(left.x, left.y);
-                    path.lineTo(bottomLeft.x, bottomLeft.y);
+                    path.lineTo(bottom.x, bottom.y);
                     path.close();
-                    canvas.drawPath(path, lightPaint);
+                    canvas.drawPath(path, paint2);
+                }
+                if (i < points.size() - 1 && j > 0) {
+                    Point top = points.get(i).get(j - 1);
+                    Point right = points.get(i + 1).get(j);
+                    Path path = new Path();
+                    path.setFillType(Path.FillType.EVEN_ODD);
+                    path.moveTo(point.x, point.y);
+                    path.lineTo(top.x, top.y);
+                    path.lineTo(right.x, right.y);
+                    path.close();
+                    canvas.drawPath(path, paint3);
+                }
+                if (i < points.size() - 1 && j < points.get(0).size() - 1) {
+                    Point bottom = points.get(i).get(j + 1);
+                    Point right = points.get(i + 1).get(j);
+                    Path path = new Path();
+                    path.setFillType(Path.FillType.EVEN_ODD);
+                    path.moveTo(point.x, point.y);
+                    path.lineTo(bottom.x, bottom.y);
+                    path.lineTo(right.x, right.y);
+                    path.close();
+                    canvas.drawPath(path, paint4);
                 }
             }
         }
@@ -151,7 +152,7 @@ public class MotifView extends View {
     private void drawPoints(Canvas canvas) {
         for (List<Point> column : points) {
             for (Point point : column) {
-                canvas.drawCircle(point.x, point.y, 4, darkPaint);
+                canvas.drawCircle(point.x, point.y, 4, blackPaint);
             }
         }
     }
