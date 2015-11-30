a9c435ed347503e5abc66fd1c2986a984fbcea3a
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2015-11-30 10:34:46 +0100

    Add missing private statements

--- app/src/main/java/fr/masciulli/motif/MainActivity.java
+++ app/src/main/java/fr/masciulli/motif/MainActivity.java
@@ -13,8 +13,8 @@ public class MainActivity extends AppCompatActivity {
     private int[] teal = {0xFF009688, 0xFF26A69A, 0xFF4DB6AC, 0xFF80CBC4};
     private int[] blue = {0xFF3F51B5, 0xFF5C6BC0, 0xFF7986CB, 0xFF9FA8DA};
     private int[] brown = {0xFF795548, 0xFF8D6E63, 0xFFA1887F, 0xFFBCAAA4};
-    int[][] colors = {red, teal, blue, brown};
-    int colorIndex = 0;
+    private int[][] colors = {red, teal, blue, brown};
+    private int colorIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
